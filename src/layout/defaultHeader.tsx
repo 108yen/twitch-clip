78e596e8527164e8536c75c1298b29d386436763
kazuki <kazuking.1911@gmail.com>
2023-04-01 21:34:40 +0900

    ヘッダーにメニュー追加

--- src/layout/defaultHeader.tsx
+++ src/layout/defaultHeader.tsx
@@ -1,6 +1,6 @@
 import { NoDecorationTypography } from "@/components/styledui";
 import { HexagonOutlined } from "@mui/icons-material";
-import { AppBar, Box, Stack,  Toolbar } from "@mui/material";
+import { AppBar, Box, Stack, Toolbar, Tooltip } from "@mui/material";
 import Link from "next/link";
 import NotificationMenu from "./notificationMenu";
 
@@ -40,6 +40,46 @@ export default function DefaultHeader() {
                                 Twitch clip ranking
                             </NoDecorationTypography>
                         </Link>
+                        <Stack
+                            direction="row"
+                            alignItems="center"
+                            spacing={2}
+                            marginX={5}
+                            sx={{
+                                display: { xs: 'none', sm: 'flex' },
+                            }}
+                        >
+                            <Link
+                                href='/past'
+                                style={{
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                <Tooltip title="過去の年別ランキング">
+                                    <NoDecorationTypography
+                                        variant="body1"
+                                        noWrap
+                                    >
+                                        Past ranking
+                                    </NoDecorationTypography>
+                                </Tooltip>
+                            </Link>
+                            <Link
+                                href='/streamers'
+                                style={{
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                <Tooltip title="チャンネル一覧">
+                                    <NoDecorationTypography
+                                        variant="body1"
+                                        noWrap
+                                    >
+                                        Channels
+                                    </NoDecorationTypography>
+                                </Tooltip>
+                            </Link>
+                        </Stack>
                     </Stack>
                     <Box
                         sx={{ flexGrow: 1 }}
