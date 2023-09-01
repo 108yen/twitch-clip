b0d5f479c2b7b81f02ddcc70396e9c54890dd9be
kazuki <kazuking.1911@gmail.com>
2023-09-01 23:00:53 +0900

    rootのpage表示まで

---
+++ src/app/layout.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react'
+import { Providers } from '@/components/providers';
+
+export const metadata = {
+    title: {
+        template: 'Twitchクリップランキング | %s',
+    },
+    description: 'Twitch(ツイッチ)クリップの再生数ランキング。※すべての配信者の集計ではありません。',
+    openGraph: {
+        type: 'website',
+        title: 'Twitchクリップランキング',
+        description: 'Twitch(ツイッチ)クリップの再生数ランキング。※すべての配信者の集計ではありません。',
+        siteName: 'Twitch clip ranking',
+        url: 'https://www.twitchclipsranking.com/',
+        images: [
+            {
+                url: "https://www.twitchclipsranking.com/android-chrome-512x512.png",
+                width: 512,
+                height: 512,
+                alt: 'Twitchクリップランキング',
+                type: 'image/png',
+            }
+        ]
+    },
+    twitter: {
+        handle: '@handle',
+        site: '@site',
+        cardType: "summary_large_image",
+    },
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+
+    return (
+        <html lang="ja">
+            <body>
+                <main>
+                    <Providers>{children}
+                    </Providers>
+                </main>
+            </body>
+        </html>
+    )
+}
---
+++ src/components/providers.tsx
@@ -0,0 +1,53 @@
+'use client'
+import { CssBaseline, ThemeProvider, createTheme, useMediaQuery } from "@mui/material"
+import { useAtom } from "jotai";
+import { isDarkModeAtom } from "./Atoms";
+import { themeOptions } from "@/theme";
+import {  useEffect, useRef } from 'react'
+import { event } from "nextjs-google-analytics";
+import { usePathname } from 'next/navigation'
+import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+    const [isDarkMode] = useAtom(isDarkModeAtom);
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)', {
+        noSsr: true,
+    });
+    const theme = createTheme(themeOptions(
+        isDarkMode == undefined ? prefersDarkMode : isDarkMode
+
+    ));
+    //GA
+    const useNavigationEvent = (onPathnameChange: (pathname: string | null) => void) => {
+        const pathname = usePathname(); // Get current route
+
+        // Save pathname on component mount into a REF
+        const savedPathNameRef = useRef(pathname);
+
+        useEffect(() => {
+            // If REF has been changed, do the stuff
+            if (savedPathNameRef.current !== pathname) {
+                onPathnameChange(pathname);
+                // Update REF
+                savedPathNameRef.current = pathname;
+            }
+        }, [pathname, onPathnameChange]);
+    };
+    useNavigationEvent((pathname) => {
+        event("page_view", {
+            page_path: pathname,
+        });
+    })
+    return (
+        <ThemeProvider theme={theme}>
+            {/* Vercel Analytics */}
+            <Analytics />
+            {/* Google Analytics */}
+            <GoogleAnalytics debugMode trackPageViews gaMeasurementId={process.env.NEXT_PUBLIC_GA_ID} />
+
+            <CssBaseline />
+            {children}
+        </ThemeProvider>
+    )
+}
