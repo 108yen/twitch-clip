c36e29e87a557143bfe120f8d25ba06cc7d0c2e1
kazuki <kazuking.1911@gmail.com>
2023-09-02 16:07:42 +0900

    最初のページの表示
    ダークモードの切り替えも確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,51 @@
+'use client'
+import { currentStreamerIdAtom } from "@/components/Atoms";
+import { ClipListLayout } from "@/layout/clipListLayout";
+import { ClipViewLayout } from "@/layout/clipViewLayout";
 import DefaultHeader from "@/layout/defaultHeader";
+import { Clip } from "@/models/clip";
+import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    //set clicked clip
+    const [currentClip, setCurrentClip] = useState<Clip | undefined>();
+    function handleSetClip(clip: Clip) {
+        setCurrentClip(clip);
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }
+    const [, setCurrentStreamerId] = useAtom(currentStreamerIdAtom);
+    //to return listview from view layout
+    function returnListView() {
+        setCurrentClip(undefined);
+    }
+
+    useEffect(() => {
+        setCurrentStreamerId('summary');
+
+        history.pushState(null, '', null);
+        window.addEventListener('popstate', returnListView, false);
+        return () => {
+            window.removeEventListener('popstate', returnListView, false);
+        };
+    }, []);
+
     return (
         <>
             <DefaultHeader />
+            {
+                currentClip === undefined
+                    ? <ClipListLayout
+                        setClickedClip={handleSetClip}
+                    />
+                    : <ClipViewLayout
+                        currentClip={currentClip!}
+                        setClickedClip={handleSetClip}
+                    />
+            }
         </>
     );
 }
