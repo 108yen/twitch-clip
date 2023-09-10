d5a7fdc344f7dff3d199ac4cceb3d3f8facef7f0
kazuki <kazuking.1911@gmail.com>
2023-09-10 20:27:11 +0900

    app checkの初期化実装

--- src/firebase/client.tsx
+++ src/firebase/client.tsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider, getToken } from 'firebase/app-check'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,3 +22,27 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+
+// AppCheck
+// FIREBASE_APPCHECK_DEBUG_TOKEN の定義(TypeScript用)
+declare global {
+    var FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | string | undefined
+}
+
+if (typeof document !== 'undefined') {
+    // 1.デバック環境用設定
+    if (process.env.NODE_ENV === 'development') {
+        // デバッグ用文字列の生成
+        window.self.FIREBASE_APPCHECK_DEBUG_TOKEN = true
+    }
+    // 2.AppCheck 初期化
+    const appCheck = initializeAppCheck(app, {
+        provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA!),
+        isTokenAutoRefreshEnabled: true,
+    })
+    // 3.AppCheck　結果 ＆ トークン確認
+    getToken(appCheck)
+        .catch((error) => {
+            console.log(error.message)
+        })
+}
