f65135423a2b6f2ea81d060edb2c5c9e06284990
kazuki <kazuking.1911@gmail.com>
2023-09-23 18:32:53 +0900

    streamersConverterのassert追加

--- src/firebase/converters/streamersConverter.tsx
+++ src/firebase/converters/streamersConverter.tsx
@@ -1,14 +1,34 @@
-import { DocumentData, FirestoreDataConverter, QueryDocumentSnapshot } from "firebase/firestore"
+import assert from 'assert'
 
-import { Streamer } from "@/models/streamer"
+import {
+    DocumentData,
+    FirestoreDataConverter,
+    QueryDocumentSnapshot
+} from 'firebase/firestore'
 
+import { Streamer } from '@/models/streamer'
 
-export const streamersConverter: FirestoreDataConverter<{ streamers: Array<Streamer> }> = {
+export const streamersConverter: FirestoreDataConverter<{
+    streamers: Array<Streamer>
+}> = {
     fromFirestore(qds: QueryDocumentSnapshot): { streamers: Array<Streamer> } {
-        const data = qds.data() as { streamers: Array<Streamer> }
-        return data
+        const data = qds.data()
+        assert(
+            typeof data.streamers !== `undefined`,
+            new Error(
+                `streamersConverter/fromFirestore: data.streamers is undefined`
+            )
+        )
+
+        const result: Array<Streamer> = []
+        for (const i in data.streamers) {
+            const streamer = data.streamers[i] as Streamer
+            result.push(streamer)
+        }
+
+        return { streamers: result }
     },
     toFirestore(doc: { streamers: Array<Streamer> }): DocumentData {
         return doc
-    },
+    }
 }
