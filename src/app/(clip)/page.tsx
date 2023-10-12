9aaf0bc5ddb7035ffeb36c8052efc65a6fd8520d
kazuki <kazuking.1911@gmail.com>
2023-10-12 16:51:44 +0900

    homeにもrevalidate追加

--- src/app/(clip)/page.tsx
+++ src/app/(clip)/page.tsx
@@ -4,6 +4,8 @@ import getClips from '../../firebase/server/clips'
 
 import ClipPageTemplate from './_Component/ClipPageTemplate'
 
+export const revalidate = 60
+
 export default async function Home() {
     // const clipDoc = await cacheClipDoc(`summary`)
     const id = `summary`
