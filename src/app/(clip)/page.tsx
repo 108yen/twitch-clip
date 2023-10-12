fd10e02d53ae023653d70b95999e1c409e06a7c4
kazuki <kazuking.1911@gmail.com>
2023-10-12 16:46:09 +0900

    summaryだけunstable_cache直書き

--- src/app/(clip)/page.tsx
+++ src/app/(clip)/page.tsx
@@ -1,9 +1,22 @@
-import { cacheClipDoc } from '../../components/unstableCache'
+import { unstable_cache } from 'next/cache'
+
+import getClips from '../../firebase/server/clips'
 
 import ClipPageTemplate from './_Component/ClipPageTemplate'
 
 export default async function Home() {
-    const clipDoc = await cacheClipDoc(`summary`)
+    // const clipDoc = await cacheClipDoc(`summary`)
+    const id = `summary`
+    const clipDoc = await unstable_cache(
+        async () => {
+            console.log(`info: cached ${id} clipDoc at ${new Date()}`)
+            return await getClips(id)
+        },
+        [id],
+        {
+            revalidate: 60
+        }
+    )()
 
     return <ClipPageTemplate clipDoc={clipDoc} />
 }
