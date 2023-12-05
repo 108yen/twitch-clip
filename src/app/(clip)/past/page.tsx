6b7f3782be7922d3ee81993319560709cb185602
kazuki <kazuking.1911@gmail.com>
2023-12-05 17:15:14 +0900

    ページのrevalidate削除

--- src/app/(clip)/past/page.tsx
+++ src/app/(clip)/past/page.tsx
@@ -1,8 +1,6 @@
 import getClips from '../../../firebase/server/clips'
 import ClipPageTemplate from '../_Component/ClipPageTemplate'
 
-export const revalidate = 1200 //20minutes
-
 export default async function PastRanking() {
     const clipDoc = await getClips(`past_summary`)
 
