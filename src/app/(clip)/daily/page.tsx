4a33551ee204f2bb00bf000876223975c65f5c9b
kazuki <kazuking.1911@gmail.com>
2023-12-09 00:36:32 +0900

    ページの再検証を1分に設定

--- src/app/(clip)/daily/page.tsx
+++ src/app/(clip)/daily/page.tsx
@@ -1,6 +1,8 @@
 import getClips from '../../../firebase/server/clips'
 import ClipPageTemplate from '../_Component/ClipPageTemplate'
 
+export const revalidate = 60 // 1minutes
+
 export default async function PastRanking() {
     const clipDoc = await getClips(`daily`)
 
