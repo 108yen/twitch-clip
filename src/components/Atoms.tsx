7fe6dd10779068291fba6a9f9337e2f3f23ab942
kazuki <kazuking.1911@gmail.com>
2023-12-24 18:18:29 +0900

    jotaiのatomWithStorageに変更

--- src/components/Atoms.tsx
+++ src/components/Atoms.tsx
@@ -1,19 +1,6 @@
-import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 
-const isDarkMode = atom<boolean | undefined>(undefined)
-export const isDarkModeAtom = atom(
-    (get) => {
-        const isDarkModeState = get(isDarkMode)
-        if (isDarkModeState != undefined) return isDarkModeState
-        // local check
-        if (typeof window === `undefined`) return false
-        const localStrageState = localStorage.getItem(`isDarkMode`)
-        if (localStrageState != undefined) return localStrageState == `true`
-        return undefined
-    },
-    (_get, set, update: boolean) => {
-        const stateToString = update ? `true` : `false`
-        localStorage.setItem(`isDarkMode`, stateToString)
-        set(isDarkMode, update)
-    }
+export const isDarkModeAtom = atomWithStorage<boolean | undefined>(
+    `isDarkMode`,
+    undefined
 )
