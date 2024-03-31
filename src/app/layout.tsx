fd0f22fa2b736db2444bb35f7bedd062d45f7758
108yen <kazuking.1911@gmail.com>
2024-03-31 15:15:32 +0900

    chore(app): remove adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 // import { headers } from 'next/headers'
-import { ReactNode } from 'react'
+import { headers } from 'next/headers'
+import { ReactNode, Suspense } from 'react'
 
 import DefaultHeader from '@/app/_Component/defaultHeader'
+import GoogleAnalytics from '@/components/googleAnalytics/GoogleAnalytics'
 
 import generateTemplateMetadata from '../utils/generateTemplateMetadata'
 
@@ -12,19 +14,19 @@ export function generateMetadata() {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    // const header = headers()
-    // const ip = (header.get(`x-forwarded-for`) ?? ``).split(`,`)[0]
+    const header = headers()
+    const ip = (header.get(`x-forwarded-for`) ?? ``).split(`,`)[0]
 
     return (
         <html lang='ja'>
             <head>
-                {/* <Suspense> */}
-                {/*     <GoogleAnalytics */}
-                {/*         debugMode={process.env.NEXT_PUBLIC_DEBUG_MODE == `1`} */}
-                {/*         clientIp={ip} */}
-                {/*     /> */}
-                {/*     <Adsbygoogle /> */}
-                {/* </Suspense> */}
+                <Suspense>
+                    <GoogleAnalytics
+                        debugMode={process.env.NEXT_PUBLIC_DEBUG_MODE == `1`}
+                        clientIp={ip}
+                    />
+                    {/* <Adsbygoogle /> */}
+                </Suspense>
             </head>
             <body>
                 <ThemeRegistry options={{ key: `css`, prepend: true }}>
