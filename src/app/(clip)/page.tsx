357ad45f762bdda29386e1e6144615f343cf55f3
108yen <kazuking.1911@gmail.com>
2024-03-31 14:59:59 +0900

    chore(app): remove adsense and GA

--- src/app/(clip)/page.tsx
+++ src/app/(clip)/page.tsx
@@ -2,6 +2,8 @@ import getClips from '../../firebase/server/clips'
 
 import ClipPageTemplate from './_Component/ClipPageTemplate'
 
+export const revalidate = 600
+
 export default async function Home() {
     const clipDoc = await getClips(`summary`)
     console.log(`revalidated page`)
