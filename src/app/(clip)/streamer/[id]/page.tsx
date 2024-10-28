8c8af724bdcdeaa014dc192ead6b19dc5e1f1402
108yen <kazuking.1911@gmail.com>
2024-10-28 13:53:05 +0900

    chore: enable prefetch

--- src/app/(clip)/streamer/[id]/page.tsx
+++ src/app/(clip)/streamer/[id]/page.tsx
@@ -1,15 +1,7 @@
-import getClips from "@/firebase/server/clips"
-import getStreamers from "@/firebase/server/streamers"
+import { unstable_getClips } from "@/firebase/server"
 import { StreamerClipPage } from "@/templates"
 import generateTemplateMetadata from "@/utils/generate-template-metadata"
 import { Metadata } from "next"
-
-export async function generateStaticParams() {
-  if (process.env.VERCEL_ENV != "production") return []
-
-  const streamers = await getStreamers()
-  return streamers.slice(0, 100).map((streamer) => ({ id: streamer.id }))
-}
 
 export async function generateMetadata({
   params,
@@ -17,7 +9,7 @@ export async function generateMetadata({
   params: { id: string }
 }): Promise<Metadata> {
   const id = params.id
-  const clipDoc = await getClips(id)
+  const clipDoc = await unstable_getClips(id)
   const streamerInfo = clipDoc.streamerInfo
 
   return generateTemplateMetadata({ caption: streamerInfo?.display_name })
@@ -25,7 +17,7 @@ export async function generateMetadata({
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id
-  const clipDoc = await getClips(id)
+  const clipDoc = await unstable_getClips(id)
 
   return <StreamerClipPage clipDoc={clipDoc} />
 }
