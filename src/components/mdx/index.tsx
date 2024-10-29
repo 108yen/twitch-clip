398fcbdbeca9e8256b62d6db09a2b7b587c4b64c
108yen <kazuking.1911@gmail.com>
2024-10-29 14:08:58 +0900

    chore: updated `code` element

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Code,
   DiscList,
   Heading,
   ListItem,
@@ -9,28 +10,8 @@ import { MDXComponents } from "mdx/types"
 import Link from "next/link"
 
 export const components: MDXComponents = {
-  a: (props) => (
-    <UILink
-      as={Link}
-      target="_blank"
-      {...props}
-      _hover={{ textDecoration: "none" }}
-    />
-  ),
-  code: (props) => (
-    <Text
-      as="code"
-      bg={["gray.50", "gray.800"]}
-      color={["gray.800", "gray.50"]}
-      fontFamily="mono"
-      fontSize="sm"
-      lineHeight="6"
-      minH="6"
-      px="2"
-      rounded="md"
-      {...props}
-    />
-  ),
+  a: (props) => <UILink as={Link} target="_blank" {...props} />,
+  code: (props) => <Code {...props} />,
   h1: (props) => <Heading as="h1" mb="6" mt="12" size="3xl" {...props} />,
   h2: (props) => <Heading as="h2" mb="6" mt="12" {...props} />,
   h3: (props) => <Heading as="h3" mb="6" mt="8" size="md" {...props} />,
