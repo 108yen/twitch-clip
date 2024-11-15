d0bf722e8db8a7d18e43b123aa8196bd1227eef6
108yen <kazuking.1911@gmail.com>
2024-11-16 00:27:40 +0900

    chore: updated ignore build step

--- ignore-build-step.sh
+++ ignore-build-step.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_GIT_PULL_REQUEST_ID: $VERCEL_GIT_PULL_REQUEST_ID"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "release" ]] ; then
+if [[ "$VERCEL_GIT_PULL_REQUEST_ID" != "" || "$VERCEL_GIT_COMMIT_REF" == "release" ]] ; then
   echo "✅ - Build can proceed"
   exit 1;
 else
