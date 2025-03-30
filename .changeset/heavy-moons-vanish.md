d70f191b05b8898aa26b60957081b2bb891ec3a6
108yen <kazuking.1911@gmail.com>
2025-03-30 14:34:36 +0900

    refactor: updated page prop types

---
+++ .changeset/heavy-moons-vanish.md
@@ -0,0 +1,5 @@
+---
+"twitch-clip": patch
+---
+
+Refactored page prop types
--- src/app/(clip)/streamer/[id]/page.tsx
+++ src/app/(clip)/streamer/[id]/page.tsx
@@ -2,15 +2,16 @@ import { STREAMERS } from "@/constant/streamers"
 import { unstable_getClips } from "@/firebase/server"
 import { StreamerClipPage } from "@/ui/templates"
 import generateTemplateMetadata from "@/utils/generate-template-metadata"
-import { Metadata } from "next"
+import { Metadata, NextPageProps } from "next"
 
-interface Props {
-  params: Promise<{ id: string }>
+interface Params {
+  id: string
 }
 
-export async function generateMetadata(props: Props): Promise<Metadata> {
-  const params = await props.params
-  const { id } = params
+export async function generateMetadata({
+  params,
+}: NextPageProps<Params>): Promise<Metadata> {
+  const { id } = await params
 
   const streamer = STREAMERS.find(({ id: _id }) => _id == id)
 
@@ -21,9 +22,8 @@ export async function generateMetadata(props: Props): Promise<Metadata> {
   return generateTemplateMetadata({ caption: streamer.display_name })
 }
 
-export default async function Page(props: Props) {
-  const params = await props.params
-  const { id } = params
+export default async function Page({ params }: NextPageProps<Params>) {
+  const { id } = await params
 
   const clipDoc = await unstable_getClips(id)
 
