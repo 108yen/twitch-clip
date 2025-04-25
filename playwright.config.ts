c067ea472fee428644e66083374cd438fdf84407
108yen <kazuking.1911@gmail.com>
2025-04-25 22:12:35 +0900

    feat(ci): added set commit status steps

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,7 +44,7 @@ export default defineConfig({
     },
   ],
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "html",
+  reporter: "html",
   retries: 2,
   testDir: "./e2e",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
